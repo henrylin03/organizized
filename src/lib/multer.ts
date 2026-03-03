f5fe4ffffcd06d9e560ef2ebb79cba623d11aaf0
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-03-03 16:29:22 +1100

    feat(multer): use memoryStorage for multer so that data is not written to disk first. move the helper function to the utils/helpers file.

--- src/lib/multer.ts
+++ src/lib/multer.ts
@@ -1,21 +1,5 @@
 import multer from "multer";
 
-const generateUniqueFilename = (file: Express.Multer.File): string => {
-	const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
-
-	const originalNameSplit = file.originalname.split(".");
-	const fileExtension = originalNameSplit.pop();
-	const originalNameWithoutFileExtension = originalNameSplit.join("");
-
-	return `${originalNameWithoutFileExtension} - ${uniqueSuffix}.${fileExtension}`;
-};
-
-const storage = multer.diskStorage({
-	destination: "./uploads",
-	filename: (_req, file, callback) => {
-		const uniqueFilename = generateUniqueFilename(file);
-		callback(null, uniqueFilename);
-	},
-});
+const storage = multer.memoryStorage();
 
 export const upload = multer({ storage });
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -3,3 +3,13 @@ export const getFileExtension = (file: Express.Multer.File): `.${string}` => {
 	const fileExtension = originalNameSplit.pop();
 	return `.${fileExtension}`;
 };
+
+export const generateUniqueFilename = (file: Express.Multer.File): string => {
+	const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+
+	const originalNameSplit = file.originalname.split(".");
+	const fileExtension = originalNameSplit.pop();
+	const originalNameWithoutFileExtension = originalNameSplit.join("");
+
+	return `${originalNameWithoutFileExtension} - ${uniqueSuffix}.${fileExtension}`;
+};
