8d5d8cbc5b27f6ab8c2149173d3db6a9677580f3
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-03-07 14:53:18 +1100

    feat(validateSignUp): remove the check for confirm password
    dropbox didn't have this at all

--- src/validators/validateSignUp.ts
+++ src/validators/validateSignUp.ts
@@ -18,16 +18,14 @@ export const validateSignUpForm = [
 		.isLength({ min: 2, max: 64 })
 		.withMessage("Last name must be between 2 and 64 characters"),
 
-	body("username")
+	body("email")
 		.trim()
 		.notEmpty()
-		.withMessage(
-			"Username is a required field. Please enter your email address.",
-		)
+		.withMessage("Please enter your email address.")
 		.isEmail()
 		.withMessage("Please enter a valid email address")
 		.isLength({ max: 256 })
-		.withMessage("Please enter an email address that is max 256 characters")
+		.withMessage("Please enter an email address that is ≤ 256 characters")
 		.custom(async (inputtedUsername: string) => {
 			const user = await prisma.user.findUnique({
 				where: { username: inputtedUsername },
@@ -41,11 +39,4 @@ export const validateSignUpForm = [
 		.withMessage("Please set a password")
 		.isLength({ min: 8, max: 64 })
 		.withMessage("Password must be between 8 and 64 characters"),
-
-	body("confirmPassword")
-		.custom(
-			(inputtedConfirmPassword: string, { req }) =>
-				inputtedConfirmPassword === req.body.password,
-		)
-		.withMessage("Passwords must match"),
 ];
